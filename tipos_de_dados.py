ab227b6572542fbd9a25bc42a4b8526bea98ea9c
Wendell Costa <wendellcosta.dev@gmail.com>
2025-02-22 17:53:15 -0300

    tipos de dados em python

---
+++ tipos_de_dados.py
@@ -0,0 +1,18 @@
+# Números inteiros
+print(1 + 1);
+
+#Números de ponto flutuante
+print(1.7 + 0.3);
+
+#String
+print("Python");
+
+#Booleano
+print(True);
+print(False);
+
+#Classes
+int()
+float()
+str()
+bool()
