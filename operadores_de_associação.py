a6ae1b86a4674bfefda3b8c9e91034e93b3e104b
Wendell Costa <wendellcosta.dev@gmail.com>
2025-02-23 20:43:41 -0300

    operadores de associação

---
+++ operadores_de_associação.py
@@ -0,0 +1,13 @@
+# Os operadores de associação são operadores utilizados para verificar se um objeto está presente em uma sequência
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+print("Python" in curso)
+# >>> True
+
+print("maça" not in frutas)
+# >>> True
+
+print(200 in saques)
+# >>> False
